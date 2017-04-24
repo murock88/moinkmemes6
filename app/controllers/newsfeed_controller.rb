cecd7dfaaba9d8fdcc958928373067e0e92086ed
murock88 <devildogmurock@gmail.com>
2017-04-24 01:29:04 +0000

    added comments

--- app/controllers/newsfeed_controller.rb
+++ app/controllers/newsfeed_controller.rb
@@ -2,9 +2,9 @@ class NewsfeedController < ApplicationController
     
   def home
     following = Array.new
-    for @f in current_user.following do
-      following.push(@f.id)
-    end
+        for @f in current_user.following do
+         following.push(@f.id)
+        end
 
     @memes = Meme.where("user_id IN (?)", following)
     @newMeme = Meme.new
